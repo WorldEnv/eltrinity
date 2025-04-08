8a2fcc7d7c0372a607a8e3a9947b6a61c45852b6
trindadedev13 <trindadedev13@gmail.com>
2025-04-08 03:16:15 -0300

    projects-list

---
+++ src/app/src/main/java/dev/trindadedev/eltrinity/beans/ProjectBean.java
@@ -0,0 +1,53 @@
+package dev.trindadedev.eltrinity.beans;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import dev.trindadedev.eltrinity.utils.PrintUtil;
+
+public class ProjectBean extends BaseBean implements Parcelable {
+  public static final Creator<ProjectBean> CREATOR =
+      new Creator<ProjectBean>() {
+        public ProjectBean createFromParcel(Parcel parcel) {
+          return new ProjectBean(parcel);
+        }
+
+        public ProjectBean[] newArray(int size) {
+          return new ProjectBean[size];
+        }
+      };
+
+  public String name;
+
+  public ProjectBean() {
+    name = "Project";
+  }
+
+  public ProjectBean(final Parcel parcel) {
+    name = parcel.readString();
+  }
+
+  public void copy(final ProjectBean other) {
+    name = other.name;
+  }
+
+  @Override
+  public Creator getCreator() {
+    return CREATOR;
+  }
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void print() {
+    PrintUtil.print(name);
+  }
+
+  @Override
+  public void writeToParcel(final Parcel parcel, final int flags) {
+    parcel.writeString(name);
+  }
+}
---
+++ src/app/src/main/java/dev/trindadedev/eltrinity/utils/function/SingleListener.java
@@ -0,0 +1,5 @@
+package dev.trindadedev.blockode.utils.function;
+
+public interface SingleListener {
+  void call();
+}
