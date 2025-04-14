0ba7ac5e7a88993c4f5a3fedd2b585b133d92620
trindadedev13 <trindadedev13@gmail.com>
2025-04-14 15:57:23 -0300

    chore: Improve Build Files

--- src/c2bsh/compile_run.sh
+++ src/c2bsh/compile_run.sh
@@ -5,6 +5,9 @@
 # it copies the source to "HOME/c2bsh" dir, compile and run.
 
 PROGRAM_NAME="c2bsh"
+PROGRAM_PATH="$HOME/binary"
+PROGRAM_FILE="$PROGRAM_PATH/$PROGRAM_NAME.bin"
+C_FILE="$HOME/testcode.c"
 
 rm -rf build
 mkdir build && cd build
@@ -13,8 +16,9 @@ cmake ../
 make
 
 if [ "$1" = "run" ]; then
-  cp $PROGRAM_NAME $HOME
-  echo "$PROGRAM_NAME copied to $HOME"
-  chmod +x $HOME/$PROGRAM_NAME
-  $HOME/$PROGRAM_NAME $HOME/testcode.c
+  mkdir $PROGRAM_PATH
+  cp $PROGRAM_NAME $PROGRAM_FILE
+  echo "$PROGRAM_NAME copied to $PROGRAM_FILE"
+  chmod +x $PROGRAM_FILE
+  $PROGRAM_FILE $C_FILE
 fi;
